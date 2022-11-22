eaa73d8a07ea6ac9ab9b1d8a84ae5c9e3837ee39
Cong Zhang <dancerphil1994@gmail.com>
2022-11-22 17:32:35 +0800

    add reproduction

--- packages/react18App/src/components/App/index.tsx
+++ packages/react18App/src/components/App/index.tsx
@@ -1,5 +1,30 @@
+import {Button, Modal} from 'antd';
+import {useCallback, useState} from "react";
+
 const App = () => {
-    return <>TODO add reproduction</>;
+    const [open, setOpen] = useState(false);
+    const handleOpen = useCallback(
+        () => setOpen(true),
+        []
+    )
+    const handleClose = useCallback(
+        () => setOpen(false),
+        []
+    )
+
+    return (
+        <>
+            <Button onClick={handleOpen}>打开弹窗</Button>
+            <div style={{height: '200vh', background: 'lightgray'}}></div>
+            <Modal
+                open={open}
+                onOk={handleClose}
+                onCancel={handleClose}
+            >
+                xxx
+            </Modal>
+        </>
+    );
 };
 
 export default App
